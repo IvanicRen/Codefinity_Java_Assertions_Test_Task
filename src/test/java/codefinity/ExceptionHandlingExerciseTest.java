01dcade05d2f9ae9fcbe468b8b74c925f64c687b
Renato Ivanic <IvanicRen@sbsit.com>
2025-06-25 17:54:47 +0200

    solved

--- src/test/java/codefinity/ExceptionHandlingExerciseTest.java
+++ src/test/java/codefinity/ExceptionHandlingExerciseTest.java
@@ -2,6 +2,9 @@ package codefinity;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class ExceptionHandlingExerciseTest {
 
     //ToDo: Pay attention to the test names
@@ -10,21 +13,29 @@ public class ExceptionHandlingExerciseTest {
 
     @Test
     public void checkAge_ShouldThrowIllegalArgumentException_WhenAgeIsLessThan18() {
-
+        ExceptionHandlingExercise test = new ExceptionHandlingExercise();
+        int age = 17;
+        assertThrows(IllegalArgumentException.class, () -> test.checkAge(age));
     }
 
     @Test
     public void checkAge_ShouldNotThrowException_WhenAgeIs18OrMore() {
-
+        ExceptionHandlingExercise test = new ExceptionHandlingExercise();
+        int age = 18;
+        assertDoesNotThrow(() -> test.checkAge(age));
     }
 
     @Test
     public void printLength_ShouldThrowNullPointerException_WhenStringIsNull() {
-
+        ExceptionHandlingExercise test = new ExceptionHandlingExercise();
+        String string = null;
+        assertThrows(NullPointerException.class, () -> test.printLength(string));
     }
 
     @Test
     public void printLength_ShouldNotThrowException_WhenStringIsNotNull() {
-
+        ExceptionHandlingExercise test = new ExceptionHandlingExercise();
+        String string = "test";
+        assertDoesNotThrow(() -> test.printLength(string));
     }
 }
